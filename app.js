1dd5a5a6b326b389769e4ad7b8ca6cc22e6a0f49
asiwajudev <olanrewajuranda@gmail.com>
2020-02-03 09:42:45 +0100

    Dice game working fine, additional modification to be done

---
+++ app.js
@@ -0,0 +1,128 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+let scores, roundScore, activePlayer, gamePlay, lastDice;
+
+init();
+
+//Roll Dice
+document.querySelector(".btn-roll").addEventListener("click", () => {
+  //Check is there is a game playing
+  if (gamePlay) {
+    //Random Number
+    let dice1 = Math.floor(Math.random() * 6) + 1;
+    let dice2 = Math.floor(Math.random() * 6) + 1;
+
+    //Display
+    showDice();
+
+    document.getElementById("dice-1").src = "dice-" + dice1 + ".png";
+    document.getElementById("dice-2").src = "dice-" + dice2 + ".png";
+
+    //Update the score if rolled number is not 1
+    if (dice === 6 && lastDice === 0) {
+      //player loses score
+      scores[activePlayer] = 0;
+      document.querySelector("#score-" + activePlayer).textContent = "0";
+      nextPlayer();
+    } else if (dice !== 1) {
+      //Add Score
+      roundScore += dice;
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
+
+    lastDice = dice;
+  }
+});
+
+//Add score to board
+document.querySelector(".btn-hold").addEventListener("click", () => {
+  if (gamePlay) {
+    //Add current score to global score
+    scores[activePlayer] += roundScore;
+
+    //Update the UI
+    document.querySelector("#score-" + activePlayer).textContent =
+      scores[activePlayer];
+
+    //Check if player won
+    if (scores[activePlayer] >= 100) {
+      document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+      hideDice();
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+      gamePlay = false;
+    }
+    //Next Player
+    nextPlayer();
+  }
+});
+
+//Restart game
+document.querySelector(".btn-new").addEventListener("click", init);
+
+//Initialization
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+  gamePlay = true;
+
+  hideDice();
+
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("winner");
+
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+
+  document.querySelector(".player-1-panel").classList.remove("active");
+}
+
+//Next Player
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById("current-0").textContent = 0;
+  document.getElementById("current-1").textContent = 0;
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  hideDice();
+}
+
+function hideDice() {
+  document.getElementById("dice-1").style.display = "none";
+  document.getElementById("dice-2").style.display = "none";
+}
+
+function showDice() {
+  document.getElementById("dice-1").style.display = "block";
+  document.getElementById("dice-2").style.display = "block";
+}
